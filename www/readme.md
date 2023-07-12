fd33fee82f26189a9dd7bf6c2f6cb2e1d09b7a63
mvllow <mvllow@icloud.com>
2023-07-12 19:22:49 +0200

    feat: restructure website with alvu

---
+++ www/readme.md
@@ -0,0 +1,13 @@
+# goblin site
+
+> Golang binaries in a curl, built by goblins
+
+## Contributing
+
+This site is built using [alvu](https://barelyhuman.github.io/alvu/00-readme).
+
+`make install` — Download dependencies
+
+`make build` - Build files with alvu and tailwindcss
+
+`make watch` - Watch files and build on change (served at [localhost:3000](http://localhost:3000))
---
+++ www/tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./pages/**/*.{html,md}"],
+	theme: {
+		extend: {
+			spacing: {
+				"page-top": "var(--page-top)",
+				"page-gutter": "var(--page-gutter)",
+				"header-height": "var(--header-height)",
+			},
+			maxWidth: {
+				content: "var(--content-width)",
+			},
+			minHeight: {
+				content: "var(--content-height)",
+			},
+			colors: {
+				base: "rgb(var(--color-base) / <alpha-value>)",
+				surface: "rgb(var(--color-surface) / <alpha-value>)",
+				overlay: "rgb(var(--color-overlay) / <alpha-value>)",
+				subtle: "rgb(var(--color-subtle) / <alpha-value>)",
+				text: "rgb(var(--color-text) / <alpha-value>)",
+				primary: "rgb(var(--color-primary) / <alpha-value>)",
+				secondary: "rgb(var(--color-secondary) / <alpha-value>)",
+			},
+			borderColor: {
+				DEFAULT: "rgb(var(--color-overlay))",
+			},
+			ringColor: {
+				DEFAULT: "rgb(var(--color-primary) / 0.2)",
+			},
+		},
+	},
+	plugins: [],
+};
