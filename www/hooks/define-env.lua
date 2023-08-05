a1f3e2151066d4f8d638d2a7f8cec1c9fe859d3b
Reaper Gelera <ahoy@barelyhuman.dev>
2023-08-05 21:12:46 +0530

    update lua for the website

--- www/hooks/define-env.lua
+++ www/hooks/define-env.lua
@@ -1,37 +1,10 @@
 local json = require("json")
-local strings = require("strings")
-
-function interp(s, tab)
-    return (s:gsub("\r?\n", " "):gsub('($%b{})', function(w) return tab[w:sub(3, -2)] or w end))
-end
-
-function file_exists(file)
-    local f = io.open(file, "rb")
-    if f then f:close() end
-    return f ~= nil
-end
-
-function lines_from(file)
-    if not file_exists(file) then return {} end
-    local lines = {}
-    for line in io.lines(file) do 
-      lines[#lines + 1] = line
-    end
-    return lines
-end
-
+local alvu = require("alvu")
 
 function Writer(filedata)
-    local source_data = json.decode(filedata);
-
-    local envLines = lines_from(".env")
     local envData = {}
 
-    for k,v in pairs(envLines) do
-        local envKeyVal = strings.split(v,"=")
-        envData[envKeyVal[1]]=envKeyVal[2]
-    end
-    
+    envData["GOBLIN_ORIGIN_URL"] = alvu.get_env(".env","GOBLIN_ORIGIN_URL")
 
     return json.encode({
 		data = {
