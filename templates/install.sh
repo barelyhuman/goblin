6d42170ab306528dec9a1bfa280e736076a28088
Morse <beats.by.morse@gmail.com>
2024-04-28 22:11:16 +0200

    Update install.sh to use goblin.run instead of old domain (#17)
    * Update install.sh

--- templates/install.sh
+++ templates/install.sh
@@ -193,7 +193,7 @@ start() {
     install "$tmp" "$prefix"
   else
     log_info "Permissions required for installation to $prefix — alternatively specify a new directory with:"
-    log_info "  $ curl -sf https://goblin.reaper.im/$pkg@$version | PREFIX=. sh"
+    log_info "  $ curl -sf https://goblin.run/$pkg@$version | PREFIX=. sh"
     sudo install "$tmp" "$prefix"
   fi
 
@@ -202,3 +202,4 @@ start() {
 }
 
 start
+
