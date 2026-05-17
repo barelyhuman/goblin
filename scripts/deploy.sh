c21a0d84ccc48a1c5bedf14ba4f4fbff2b18c2dd
reaper <ahoy@barelyhuman.dev>
2026-05-17 14:23:55 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,16 +8,16 @@ set -euxo pipefail
 mkdir -p ~/goblin
 """
 
-ssh root@143.110.182.104 "/bin/bash -c '$PREPARE_COMMANDS'"
+ssh root@goblin.run "/bin/bash -c '$PREPARE_COMMANDS'"
 
-rsync --progress goblin-latest.tar.gz root@143.110.182.104:~/goblin/
+rsync --progress goblin-latest.tar.gz root@goblin.run:~/goblin/
 
 COMMANDS="""
 set -euxo pipefail
 cd ~/goblin
 docker image load < goblin-latest.tar.gz
 docker stop \$(docker container ls --all --filter=ancestor="goblin:latest" --format "{{.ID}}")
-docker run -d -e 'ORIGIN_URL=http://count.barelyhuman.dev'  -p '3000:3000' -v='./:/usr/bin/app' goblin:latest 
+docker run -d -e 'ORIGIN_URL=http://goblin.run'  -p '3000:3000' -v='./:/usr/bin/app' goblin:latest
 """
 
-ssh root@143.110.182.104 "/bin/bash -c '$COMMANDS'"
+ssh root@goblin.run "/bin/bash -c '$COMMANDS'"
